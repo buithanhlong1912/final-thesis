4058338d7d7113e29883ce1980d909960555291a
buithanhlong1912 <buithanhlong1912@gmail.com>
2023-07-15 00:10:15 +0700

    update admin page

--- admin/app/(dashboard)/[storeId]/(routes)/orders/[orderId]/page.tsx
+++ admin/app/(dashboard)/[storeId]/(routes)/orders/[orderId]/page.tsx
@@ -15,7 +15,7 @@ const BillboardPage = async ({
     return (
         <div className="flex-col">
             <div className="flex-1 space-y-4 p-8 pt-6">
-                <BillboardForm initialData={orders} />
+                {/* <BillboardForm initialData={orders} /> */}
             </div>
         </div>
     );
