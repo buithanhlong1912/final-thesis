3d6912c335eae5c013e95a0b9217c1f73b788a98
buithanhlong1912 <buithanhlong1912@gmail.com>
2023-07-25 23:38:45 +0700

    update alert modal

--- admin/app/(dashboard)/[storeId]/(routes)/settings/components/SettingsForm.tsx
+++ admin/app/(dashboard)/[storeId]/(routes)/settings/components/SettingsForm.tsx
@@ -14,138 +14,132 @@ import { Button } from '@/components/ui/button';
 import Heading from '@/components/ui/heading';
 import { Separator } from '@/components/ui/separator';
 import {
-    Form,
-    FormControl,
-    FormField,
-    FormItem,
-    FormLabel,
-    FormMessage,
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
 } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
 import AlertModal from '@/components/modals/alert-modals';
 
 interface SettingsFormProps {
-    initialData: Store;
+  initialData: Store;
 }
 
 const formSchema = z.object({
-    name: z.string().min(1),
+  name: z.string().min(1),
 });
 
 type SettingsFormValue = z.infer<typeof formSchema>;
 
 const SettingsForm: React.FC<SettingsFormProps> = ({
-    initialData,
+  initialData,
 }) => {
-    const params = useParams();
-    const router = useRouter();
-    const [open, setOpen] = useState(false);
-    const [loading, setLoading] = useState(false);
+  const params = useParams();
+  const router = useRouter();
+  const [open, setOpen] = useState(false);
+  const [loading, setLoading] = useState(false);
 
-    const form = useForm<SettingsFormValue>({
-        resolver: zodResolver(formSchema),
-        defaultValues: initialData,
-    });
+  const form = useForm<SettingsFormValue>({
+    resolver: zodResolver(formSchema),
+    defaultValues: initialData,
+  });
 
-    const onSubmit = async (data: SettingsFormValue) => {
-        try {
-            setLoading(true);
-            await axios.patch(
-                `/api/stores/${params.storeId}`,
-                data
-            );
-            router.refresh();
-            toast.success('Cập nhật thông tin thành công!');
-        } catch (err) {
-            toast.error('Lỗi khi cập nhật!');
-        } finally {
-            setLoading(false);
-        }
-    };
+  const onSubmit = async (data: SettingsFormValue) => {
+    try {
+      setLoading(true);
+      await axios.patch(
+        `/api/stores/${params.storeId}`,
+        data
+      );
+      router.refresh();
+      toast.success('Cập nhật thông tin thành công!');
+    } catch (err) {
+      toast.error('Lỗi khi cập nhật!');
+    } finally {
+      setLoading(false);
+    }
+  };
 
-    const onDelete = async () => {
-        try {
-            setLoading(true);
-            await axios.delete(
-                `/api/stores/${params.storeId}`
-            );
-            toast.success('Đã xóa thành công cửa hàng!');
-            router.refresh();
-            router.push('/');
-        } catch (err) {
-            toast.error(
-                'Hãy chắc chắn rằng đã hóa tất cả thông tin hành hóa trước khi xóa cửa hàng.'
-            );
-        } finally {
-            setLoading(false);
-            setOpen(false);
-        }
-    };
+  const onDelete = async () => {
+    try {
+      setLoading(true);
+      await axios.delete(`/api/stores/${params.storeId}`);
+      toast.success('Đã xóa thành công cửa hàng!');
+      router.refresh();
+      router.push('/');
+    } catch (err) {
+      toast.error(
+        'Hãy chắc chắn rằng đã hóa tất cả thông tin hành hóa trước khi xóa cửa hàng.'
+      );
+    } finally {
+      setLoading(false);
+      setOpen(false);
+    }
+  };
 
-    return (
-        <>
-            <AlertModal
-                isOpen={open}
-                onClose={() => {
-                    setOpen(false);
-                }}
-                onConfirm={onDelete}
-                loading={loading}
+  return (
+    <>
+      <AlertModal
+        isOpen={open}
+        onClose={() => {
+          setOpen(false);
+        }}
+        onConfirm={onDelete}
+        loading={loading}
+      />
+      <div className="flex items-center justify-between">
+        <Heading
+          title="Cài đặt"
+          description="Quản lý nội dung cửa hàng"
+        />
+        <Button
+          disabled={loading}
+          variant="destructive"
+          size="icon"
+          onClick={() => setOpen(true)}
+        >
+          <Trash className="h-4 w-4" />
+        </Button>
+      </div>
+      <Separator />
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="space-y-8 w-full"
+        >
+          <div className="grid grid-cols-3 gap-8">
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input
+                      disabled={loading}
+                      placeholder="Tên cửa hàng"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
             />
-            <div className="flex items-center justify-between">
-                <Heading
-                    title="Cài đặt"
-                    description="Quản lý nội dung cửa hàng"
-                />
-                <Button
-                    disabled={loading}
-                    variant="destructive"
-                    size="icon"
-                    onClick={() => setOpen(true)}
-                >
-                    <Trash className="h-4 w-4" />
-                </Button>
-            </div>
-            <Separator />
-            <Form {...form}>
-                <form
-                    onSubmit={form.handleSubmit(onSubmit)}
-                    className="space-y-8 w-full"
-                >
-                    <div className="grid grid-cols-3 gap-8">
-                        <FormField
-                            control={form.control}
-                            name="name"
-                            render={({ field }) => (
-                                <FormItem>
-                                    <FormLabel>
-                                        Name
-                                    </FormLabel>
-                                    <FormControl>
-                                        <Input
-                                            disabled={
-                                                loading
-                                            }
-                                            placeholder="Tên cửa hàng"
-                                            {...field}
-                                        />
-                                    </FormControl>
-                                    <FormMessage />
-                                </FormItem>
-                            )}
-                        />
-                    </div>
-                    <Button
-                        disabled={loading}
-                        className="ml-auto"
-                        type="submit"
-                    >
-                        Lưu thay đổi
-                    </Button>
-                </form>
-            </Form>
-        </>
-    );
+          </div>
+          <Button
+            disabled={loading}
+            className="ml-auto"
+            type="submit"
+          >
+            Lưu thay đổi
+          </Button>
+        </form>
+      </Form>
+    </>
+  );
 };
 
 export default SettingsForm;
--- admin/components/modals/alert-modals.tsx
+++ admin/components/modals/alert-modals.tsx
@@ -5,58 +5,55 @@ import Modal from '../ui/modal';
 import { Button } from '../ui/button';
 
 interface AlertModalProps {
-    isOpen: boolean;
-    onClose: () => void;
-    onConfirm: () => void;
-    loading: boolean;
-    title: string;
+  isOpen: boolean;
+  onClose: () => void;
+  onConfirm: () => void;
+  loading: boolean;
+  title?: string;
 }
 
 const AlertModal: React.FC<AlertModalProps> = ({
-    isOpen,
-    onClose,
-    onConfirm,
-    loading,
-    title,
+  isOpen,
+  onClose,
+  onConfirm,
+  loading,
+  title,
 }) => {
-    const [isMounted, setIsMounted] = useState(false);
+  const [isMounted, setIsMounted] = useState(false);
 
-    useEffect(() => {
-        setIsMounted(true);
-    }, []);
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
 
-    if (!isMounted) {
-        return null;
-    }
+  if (!isMounted) {
+    return null;
+  }
 
-    return (
-        <Modal
-            title={
-                title ||
-                'Bạn có chắc chắn muốn xóa cửa hàng?'
-            }
-            description="Hành động này không thể hoàn tác."
-            isOpen={isOpen}
-            onClose={onClose}
+  return (
+    <Modal
+      title={title || 'Bạn có chắc chắn muốn xóa cửa hàng?'}
+      description="Hành động này không thể hoàn tác."
+      isOpen={isOpen}
+      onClose={onClose}
+    >
+      <div className="pt-6 space-x-2 flex items-center justify-end w-full">
+        <Button
+          disabled={loading}
+          variant="outline"
+          onClick={onClose}
         >
-            <div className="pt-6 space-x-2 flex items-center justify-end w-full">
-                <Button
-                    disabled={loading}
-                    variant="outline"
-                    onClick={onClose}
-                >
-                    Hủy
-                </Button>
-                <Button
-                    disabled={loading}
-                    variant="destructive"
-                    onClick={onConfirm}
-                >
-                    Tiếp tục
-                </Button>
-            </div>
-        </Modal>
-    );
+          Hủy
+        </Button>
+        <Button
+          disabled={loading}
+          variant="destructive"
+          onClick={onConfirm}
+        >
+          Tiếp tục
+        </Button>
+      </div>
+    </Modal>
+  );
 };
 
 export default AlertModal;
